53fa5b9ba57b0cc141f442ff5d5a3fe5aaf411b2
Marius Bongarts <mb709027@fh-muenster.de>
2019-07-11 01:10:22 +0200

    Added css properties to README

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -4,15 +4,17 @@ import { classMap } from 'lit-html/directives/class-map';
 const componentCSS = require('./app.component.scss');
 
 /**
- * Allow users to take actions, and make choices, with a single tap.
+ * Modern button
  * @slot - Default content.
+ * @cssprop --color - Text color
  * @cssprop --bg-color - Background color
  * @cssprop --bg-color-hover - Background color when hovered
- * @cssprop --primary-color-shadow - Shadow color when focused
- * @cssprop --button-letter-spacing - Letter spacing
- * @cssprop --button-letter-spacing - Letter spacing
- * @cssprop --button-letter-spacing - Letter spacing
- * @cssprop --button-letter-spacing - Letter spacing
+ * @cssprop --button-color-shadow- Shadow color when not focused
+ * @cssprop --button-color-shadow-focus - Shadow color when focused
+ * @cssprop --border-radius - Border radius
+ * @cssprop --height - Height of button
+ * @cssprop --width - Width of button
+ * @cssprop ---margin - Margin of button
  *
  */
 @customElement('bronco-button')
@@ -29,7 +31,7 @@ export class BroncoButton extends LitElement {
   @property({ type: Boolean, reflect: false }) outline: boolean = false;
 
   /**
-   * Enabled the outline effect after clicking   *
+   * Enables the outline effect after clicking
    * @type {boolean}
    * @memberof BroncoButton
    */
@@ -46,12 +48,12 @@ export class BroncoButton extends LitElement {
   render() {
     return html`
           <button class='
-                    ${this.outline ? ' outline' : '' } ${this.outlineEffect ? 'outlineEffect' : '' }
+                              ${this.outline ? ' outline' : ''} ${this.outlineEffect ? 'outlineEffect' : ''}
             ' @click=${() => this.emit()}>
-                      <span>
-                        <slot></slot>
-                      </span>
-                    </button>
+                                <span>
+                                  <slot></slot>
+                                </span>
+                              </button>
 `;
   }
 
